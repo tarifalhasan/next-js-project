fb71560d9226bc00dc541207e3ce09d7898cb2bc
tarifalhasan <tarialhasan999@gmail.com>
2023-04-04 15:27:12 +0600

    build error fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
+
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
