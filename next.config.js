bb8dd02f61b762bd8f32ed78fecec3c82a5d114a
tarifalhasan <tarialhasan999@gmail.com>
2023-05-19 14:51:34 +0600

    replced all image in svg

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-module.exports = {
-  // other configs...
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-  // future: { webpack5: true }, // -- not needed since Next.js v11.0.0
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.(js|ts|md)x?$/] },
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            prettier: false,
-            svgo: true,
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-            titleProp: true,
-          },
-        },
-      ],
-    });
-    return config;
-  },
-};
+module.exports = nextConfig;
